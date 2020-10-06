5cf6cfd07955b36e794f6b5bfb68d5e1dcf5d309
Qarun Qadir Bissoondial <qarun80@hotmail.com>
2020-10-06 00:07:43 -0400

    Added retry test

--- src/app/todo.service.spec.ts
+++ src/app/todo.service.spec.ts
@@ -1,6 +1,7 @@
 import { TestBed } from '@angular/core/testing';
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { Todo, TodoService } from './todo.service';
+import { HttpErrorResponse } from '@angular/common/http';
 
 fdescribe('TodoService', () => {
   let service: TodoService;
@@ -35,6 +36,22 @@ fdescribe('TodoService', () => {
    
   });
 
+  fit('getSingleTodo should throw an error if 3 request attempts fail', (done) => {
+    const id = 1;
+    const errMessage = `Failed to fetch item with id ${id}`;
+    service.getSingleTodo(id).subscribe(
+      data => { fail('The request is supposed to throw an error') },
+      (error: string) => { expect(error).toEqual(errMessage); done(); },
+    );
+
+    const retryCount = 3;
+    for (let i = 0; i <= retryCount; i++) {
+      httpMock
+        .expectOne({url: 'https://jsonplaceholder.typicode.com/todos/1', method: 'GET'})
+        .flush({}, { status: 404, statusText: errMessage });
+    }
+  });
+
   it('createTodo should send a POST request and return the newly created item', (done) => {
 
     const item: Todo = {
@@ -53,6 +70,69 @@ fdescribe('TodoService', () => {
     expect(testRequest.request.method).toBe('POST');
     testRequest.flush(item);
 
+  });
+
+  describe('getAllTodos', () => {
+    it('should send a GET request and return an array of items', (done) => {
+      service.getAllTodos().subscribe(
+        (data: Todo[]) => {
+          expect(data).toBeDefined();
+          expect(Array.isArray(data)).toBe(true);
+          expect(data.length).toBe(1);
+          done();
+        },
+        (error: HttpErrorResponse) => { fail('The request was supposed to return data') }
+      );
+
+      const testRequest = httpMock.expectOne('https://jsonplaceholder.typicode.com/todos');
+      expect(testRequest.request.method).toBe('GET');
+      testRequest.flush([{ id: 1, userId: 1, title: 'Test Todo', completed: false }]);
+    });
+
+    it('should return an empty array if an Interal Server Error occurs', (done) => {
+      
+      service.getAllTodos().subscribe(
+        (data: Todo[]) => {
+          expect(data).toBeDefined();
+          expect(Array.isArray(data)).toBe(true);
+          expect(data.length).toBe(0);
+          done()
+        },
+        (error: HttpErrorResponse) => { fail('The request was supposed to return an empty array') } 
+      );
+
+      const testRequest = httpMock.expectOne('https://jsonplaceholder.typicode.com/todos');
+      expect(testRequest.request.method).toBe('GET');
+      testRequest.flush({}, { status: 500, statusText: 'Internal Server Error' });
+
+    })
+  });
+
+  describe('updateTodos', () => {
+    let displayErrorSpy: jasmine.Spy;
+
+    beforeEach(() => {
+      displayErrorSpy = spyOn(service, 'displayError');
+    })
+
+    it('should display an error message if the request is unauthorized', (done) => {
+      service
+        .updateTodo({ id: 1, userId: 1, title: 'Walk dog', completed: true })
+        .subscribe(
+          (data => {
+            expect(data).toBeNull();
+            expect(displayErrorSpy).toHaveBeenCalledWith('Unauthorized request');
+            done();
+          }),
+          (error: HttpErrorResponse) => { fail('The Observable is not supposed to fail') }
+        
+      );
+
+      const testRequest = httpMock.expectOne('https://jsonplaceholder.typicode.com/todos/1');
+      expect(testRequest.request.method).toBe('PUT');
+      testRequest.flush(null, { status: 401, statusText: 'Unauthorized request'});
+
+    });
   });
 
 });
--- src/app/todo.service.ts
+++ src/app/todo.service.ts
@@ -1,5 +1,7 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { of, throwError } from 'rxjs';
+import { catchError, retry } from 'rxjs/operators';
 
 export interface Todo {
   userId: number,
@@ -17,23 +19,49 @@ export class TodoService {
   constructor(private http: HttpClient) {}
 
   getAllTodos() {
-    return this.http.get(this.url)
+    return this.http.get(this.url).pipe(
+      
+      catchError((error: HttpErrorResponse) => {
+        
+        if (error.status === 500) {
+          return of([]);
+        }
+        
+      })
+
+    )
   }
 
   getSingleTodo(id: number) {
-    return this.http.get(`${this.url}/${id}`);
+    return this.http.get(`${this.url}/${id}`).pipe(
+      retry(3),
+      catchError(error => {
+        return throwError(`Failed to fetch item with id ${id}`)
+      })
+    )
   }
 
   createTodo(item: Todo) {
     return this.http.post(this.url, item);
   }
 
-  updateTodo(id: number, updatedItem: Todo) {
-    return this.http.put(`${this.url}/${id}`, updatedItem);
+  updateTodo(updatedItem: Todo) {
+    return this.http.put(`${this.url}/${updatedItem.id}`, updatedItem).pipe(
+      catchError((error: HttpErrorResponse) => {
+        if (error.status === 401) {
+          this.displayError(error.statusText);
+          return of(null);
+        }
+      })
+    )
   }
 
   deleteTodo(id: number) {
     return this.http.delete(`${this.url}/${id}`);
+  }
+
+  displayError(message: string) {
+    console.log(message);
   }
 
 }
